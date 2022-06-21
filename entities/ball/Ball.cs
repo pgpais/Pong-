b6dc0b8afbf1d16d5810eafbf8da09aeee57def2
Pedro Pais <pgpaisdev@gmail.com>
2022-06-21 09:59:42 +0100

    basic ball setup

---
+++ entities/ball/Ball.cs
@@ -0,0 +1,24 @@
+using System;
+using Godot;
+
+public class Ball : RigidBody2D
+{
+    // Declare member variables here. Examples:
+    // private int a = 2;
+    // private string b = "text";
+
+    // Called when the node enters the scene tree for the first time.
+    public override void _Ready()
+    {
+        //TODO: Throw ball in random direction
+        //TODO: Make ball bounce off walls
+        //TODO: Make ball bounce off paddles
+        //TODO: Throw ball in direction of scored player
+    }
+
+    //  // Called every frame. 'delta' is the elapsed time since the previous frame.
+    //  public override void _Process(float delta)
+    //  {
+    //
+    //  }
+}
