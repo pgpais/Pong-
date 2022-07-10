123a45d55d607606a39bebe2f199b9c474917258
pgpaisdev <pgpaisdev@gmail.com>
2022-07-10 12:45:19 +0100

    again?

--- entities/gameManager/GameManager.cs
+++ entities/gameManager/GameManager.cs
@@ -6,45 +6,45 @@ using Godot;
 
 public class GameManager : Node2D
 {
-    [Export]
-    public NodePath PaddleManagerPath { get; private set; }
+	[Export]
+	public NodePath PaddleManagerPath { get; private set; }
 
-    public List<Player> players = new List<Player>();
+	public List<Player> players = new List<Player>();
 
-    public GameManager()
-    {
+	public GameManager()
+	{
 
-    }
+	}
 
-    // Called when the node enters the scene tree for the first time.
-    public override void _Ready()
-    {
-        SetupPlayerColors();
+	// Called when the node enters the scene tree for the first time.
+	public override void _Ready()
+	{
+		SetupPlayerColors();
 
-        SetupSignals();
-    }
+		SetupSignals();
+	}
 
-    private void SetupPlayerColors()
-    {
-        players = new Godot.Collections.Array<Player>(GetTree().GetNodesInGroup("player")).ToList();
+	private void SetupPlayerColors()
+	{
+		players = new Godot.Collections.Array<Player>(GetTree().GetNodesInGroup("player")).ToList();
 
-        Color colorPlayer1 = players.First((p) => p.PlayerNumber == PlayerNumber.One).PlayerColor;
-        Color colorPlayer2 = players.First((p) => p.PlayerNumber == PlayerNumber.Two).PlayerColor;
+		Color colorPlayer1 = players.First((p) => p.PlayerNumber == PlayerNumber.One).PlayerColor;
+		Color colorPlayer2 = players.First((p) => p.PlayerNumber == PlayerNumber.Two).PlayerColor;
 
-        if (PaddleManagerPath != null && !PaddleManagerPath.IsEmpty())
-        {
-            var paddleManager = GetNode<PaddleManager>(PaddleManagerPath);
-            paddleManager.SetPaddleColor(PlayerNumber.One, colorPlayer1);
-            paddleManager.SetPaddleColor(PlayerNumber.Two, colorPlayer2);
-        }
-        else
-        {
-            GD.PushWarning("PaddleManagerPath is empty, will not set color");
-        }
-    }
+		if (PaddleManagerPath != null && !PaddleManagerPath.IsEmpty())
+		{
+			var paddleManager = GetNode<PaddleManager>(PaddleManagerPath);
+			paddleManager.SetPaddleColor(PlayerNumber.One, colorPlayer1);
+			paddleManager.SetPaddleColor(PlayerNumber.Two, colorPlayer2);
+		}
+		else
+		{
+			GD.PushWarning("PaddleManagerPath is empty, will not set color");
+		}
+	}
 
-    private void SetupSignals()
-    {
-        throw new NotImplementedException();
-    }
+	private void SetupSignals()
+	{
+		throw new NotImplementedException();
+	}
 }
