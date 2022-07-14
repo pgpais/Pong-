911bfaf7fcc2dbaae37856f098df4563e97807b6
Pedro Pais <pgpaisdev@gmail.com>
2022-07-14 07:08:19 +0100

    added quit button functionality

--- UI/Menus/TitleScreen.cs
+++ UI/Menus/TitleScreen.cs
@@ -8,9 +8,12 @@ public class TitleScreen : Control
     [Export]
     public NodePath PlayButtonNodePath { get; private set; }
     [Export]
+    public NodePath QuitButtonNodePath { get; private set; }
+    [Export]
     public NodePath FadeInNodePath { get; private set; }
 
     private Button playButton;
+    private Button quitButton;
     private FadeIn fadeIn;
 
     private PackedScene sceneToLoad;
@@ -19,9 +22,13 @@ public class TitleScreen : Control
     public override void _Ready()
     {
         playButton = GetNode<Button>(PlayButtonNodePath);
+        quitButton = GetNode<Button>(QuitButtonNodePath);
         fadeIn = GetNode<FadeIn>(FadeInNodePath);
 
+        playButton.GrabFocus();
+
         playButton.Connect("pressed", this, nameof(OnPlayButtonPressed));
+        quitButton.Connect("pressed", this, nameof(OnQuitButtonPressed));
         fadeIn.Connect(nameof(FadeIn.FadeFinished), this, nameof(OnFadeFinished));
     }
 
@@ -29,6 +36,16 @@ public class TitleScreen : Control
     {
         sceneToLoad = GameScene;
         fadeIn.PlayFadeIn();
+    }
+
+    private void OnQuitButtonPressed()
+    {
+        QuitApplication();
+    }
+
+    private void QuitApplication()
+    {
+        GetTree().Quit();
     }
 
     private void OnFadeFinished()
